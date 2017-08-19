f66fb055bd500a941e7ed467c99a15855815a243
brendoperes1 <brendo.peres@hotmail.com>
2017-08-19 17:14:31 -0300

    first commit

---
+++ equipe.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<!--
+	Transit by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Transit by TEMPLATED</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-layers.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-xlarge.css" />
+		</noscript>
+	</head>
+
+	<section id="banner">
+			<h2>Diretores:</h2><br><br>
+			<p>Na BIT- Empresa Junior, existem 7 integrantes:<br>
+			Murilo Francisco Schaefer  -  Presidente<br>
+			Lyssa Priscyla Scherer  -  Diretora de Projetos<br>
+			Guilherme de Oliveira Felipe  -  Diretor de Projetos<br>
+			Mateus José dos Santos  -  Tesouraria<br>
+			Hamã Candido Carvalho Lopes  -   Diretor de Marketing<br>
+			Felipe Pereira Medeiros  -  Diretor de vendas<br>
+			Carina Pasinato Dalsasso  -  Secretaria</p>
+					<img align="middle" src="images/equipe.jpg">
+		<ul class="actions">
+				<li>
+					<a href="index.html" class="button big">Inicio</a>
+				</li>
+			</ul>
+	</section>
+	
---
+++ sobre.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<!--
+	Transit by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Transit by TEMPLATED</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-layers.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-xlarge.css" />
+		</noscript>
+	</head>
+
+	<section id="banner">
+		<h2>SOBRE A EMPRESA</h2>
+		<p><br><br>
+		Criada oficiamente em 19 de abril de 2017 a empresa Bit faz parte do movimento empresas juniores <a style="color:blue" href="https://www.brasiljunior.org.br/conheca-o-mej">(MEJ)</a>, o qual tem por objetivo aumentar a familiaridade dos universitários com o mercado de trabalho com ética, compromisso com a entrega de bons resultados e entusiasmo.<br><br>
+		Sendo todas essas características utilizadas nos projetos feitos pela  BIT.  Assim, com uma proposta diferenciada vem oferecer ao mercado serviços de qualidade com um baixo custo, as novidades da área de softwares que contam com a supervisão e experiência dos professores da universidade.<br><br>
+		Com tantas vantagens, fica difícil não contatar  a Bit que conta  com um nome pequeno, fácil e bonito para ficar ao lado do nome da sua empresa e acrescentar à ela o diferencial que faltava: o entusiasmo e comprometimento de nossa equipe com a sua necessidade.</p>
+		<ul class="actions">
+					<li>
+						<a href="index.html" class="button big">Inicio</a>
+					</li>
+				</ul>
+	</section>
